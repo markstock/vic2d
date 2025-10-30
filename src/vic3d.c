fc17fa37c7d6703cac59cf387ca98bf2ef87edfc
Mark Stock <markjstock@gmail.com>
2025-10-30 18:17:19 -0400

    More local definitions of vars in vic3d

--- src/vic3d.c
+++ src/vic3d.c
@@ -1,7 +1,7 @@
 /*
  * vic3d.c - driver program for three-dimensional vortex-in-cell method of characteristics
  *
- * Copyright 2004,2005,20 Mark J. Stock <mstock@umich.edu>
+ * Copyright 2004,05,20,25 Mark J. Stock <markjstock@gmail.com>
  *
  * a 3D vortex method which uses the method of characteristics for the
  * convection step, and a single explicit step for diffusion and vorticity
@@ -29,23 +29,23 @@
 #endif
 
 #include "vicmoc.h"
+#include "utility.h"
+#include "inout.h"
 
 int Usage(char[MAXCHARS],int);
 
 int main(int argc,char **argv) {
 
    int isStam = false;			// set to true to run Stable Fluids calc
-   int i,j,k,cnt;
    int nx,ny,nz;
    int xbdry,ybdry,zbdry;
-   int ix,iy,iz;
    int step = 0;
    int ccnidx = 0;
-   int outscale;
+   int outscale = 1;
    int writeOutput = true;
    int maxStep;
    int writeevery;
-   float vmax,velsq;
+   float vmax;
    float simtime = 0.;
 
    int sc_cnt = 3;			// all 3 vorticities
@@ -56,18 +56,20 @@ int main(int argc,char **argv) {
    int sfi;
 
    // the field values
-   float reynolds;
-   float dt;
-   float bn;
-   float coeff,Re,yf,zf;
-   float px,py,pz,temp,tottemp,avgtemp = 0.0;
-   float courant,courantconst;
+   float Re = 1000.f;
+   float dt = -1.f;
+   float bn = 1.f;
+   float effRe;
+   //float yf,zf;
+   float px,py,pz,temp,tottemp;
+   float courant = 10.f;
+   float courantconst = -1.f;
    float *gravity;
-   float *freestream;
 
    float ***u[3];			// all velocities, vorticity, etc
-   float ***a[MAX_SCALARS];		// all velocities, vorticity, etc
-   float ***t[MAX_SCALARS];		// all velocities, vorticity, etc
+   float ***a[MAX_SCALARS];	// all velocities, vorticity, etc
+   float ***t[MAX_SCALARS];	// all velocities, vorticity, etc
+   float ***xo[3];			// origin points from moc
    float ke = 0.;
    float ke_last = 0.;
    float enst = 0.;
@@ -97,12 +99,6 @@ int main(int argc,char **argv) {
    xbdry = WALL;
    ybdry = WALL;
    zbdry = WALL;
-   courant = 10.0;
-   courantconst = -1.0;
-   reynolds = 1000.0;
-   outscale = 1;
-   dt = -1.0;
-   bn = 1.;
    gravity = allocate_1d_array_f(3);
    gravity[0] = 0.0;
    gravity[1] = 0.0;
@@ -117,12 +113,12 @@ int main(int argc,char **argv) {
    sc_type[0] = WX;
    sc_type[1] = WY;
    sc_type[2] = WZ;
-   for (i=3; i<MAX_SCALARS; i++) sc_type[i] = -1;
+   for (int i=3; i<MAX_SCALARS; i++) sc_type[i] = -1;
 
    // read command-line
    (void) strcpy(progname,argv[0]);
    if (argc < 1) (void) Usage(progname,0);
-   for (i=1; i<argc; i++) {
+   for (int i=1; i<argc; i++) {
 
       if (strncmp(argv[i], "-x", 2) == 0) {
          nx = atoi(argv[++i]) + 1;
@@ -139,7 +135,7 @@ int main(int argc,char **argv) {
       } else if (strncmp(argv[i], "-sc", 3) == 0) {
          use_SF = true;
       } else if (strncmp(argv[i], "-re", 3) == 0) {
-         reynolds = atof(argv[++i]);
+         Re = atof(argv[++i]);
       } else if (strncmp(argv[i], "-o", 2) == 0) {
          outscale = atoi(argv[++i]);
       } else if (strncmp(argv[i], "-cn", 3) == 0) {
@@ -164,8 +160,8 @@ int main(int argc,char **argv) {
    }
 
    // set y max bound (if square, yf=1.0, otherwise scale it as xmax=1.0
-   yf = (float)(ny-1)/(float)(nx-1);
-   zf = (float)(nz-1)/(float)(nx-1);
+   //yf = (float)(ny-1)/(float)(nx-1);
+   //zf = (float)(nz-1)/(float)(nx-1);
 
    // set time step (dt), base it on the maximum courant number (max speed of)
    //  but only if courant number is not set
@@ -189,24 +185,28 @@ int main(int argc,char **argv) {
    t[WX] = allocate_3d_array_f(nx,ny,nz);
    t[WY] = allocate_3d_array_f(nx,ny,nz);
    t[WZ] = allocate_3d_array_f(nx,ny,nz);
-   for (i=0; i<3; i++) sc_diffus[i] = 1.0/reynolds;
+   // origin points from moc
+   xo[XV] = allocate_3d_array_f(nx,ny,nz);
+   xo[YV] = allocate_3d_array_f(nx,ny,nz);
+   xo[ZV] = allocate_3d_array_f(nx,ny,nz);
+   for (int i=0; i<3; i++) sc_diffus[i] = 1.0/Re;
    if (use_SF) {
       sc_type[sc_cnt] = SF;
-      sc_diffus[sc_cnt] = 1.0/reynolds;
+      sc_diffus[sc_cnt] = 1.0/Re;
       a[sc_cnt] = allocate_3d_array_f(nx,ny,nz);
       t[sc_cnt] = allocate_3d_array_f(nx,ny,nz);
       sc_cnt++;
    }
    if (use_DF) {
       sc_type[sc_cnt] = DF;
-      sc_diffus[sc_cnt] = 1.0/reynolds;
+      sc_diffus[sc_cnt] = 1.0/Re;
       a[sc_cnt] = allocate_3d_array_f(nx,ny,nz);
       t[sc_cnt] = allocate_3d_array_f(nx,ny,nz);
       sc_cnt++;
    }
 
    // allocate space for temporary arrays
-   //for (i=0; i<MAX_SCALARS; i++) {
+   //for (int i=0; i<MAX_SCALARS; i++) {
    //   if (a[i] != NULL) {
    //      t[i] = allocate_2d_array_f(nx,ny);
    //      if (!silent) fprintf(stdout,"Array %d has diffusivity %g\n",i,sc_diffus[i]);
@@ -219,9 +219,9 @@ int main(int argc,char **argv) {
    // Set vorticity ----------------------------------
 
    // zero initial vorticity
-   for (ix=0; ix<nx; ix++) {
-      for (iy=0; iy<ny; iy++) {
-         for (iz=0; iz<nz; iz++) {
+   for (int ix=0; ix<nx; ix++) {
+      for (int iy=0; iy<ny; iy++) {
+         for (int iz=0; iz<nz; iz++) {
             a[WX][ix][iy][iz] = 0.0;
             a[WY][ix][iy][iz] = 0.0;
             a[WZ][ix][iy][iz] = 0.0;
@@ -230,11 +230,12 @@ int main(int argc,char **argv) {
    }
    if (false) {
       // create a wavy shear layer of positive vorticity (pseudo-2d)
-      for (ix=0; ix<nx; ix++) {
+      const float zf = (float)(nz-1)/(float)(nx-1);
+      for (int ix=0; ix<nx; ix++) {
          px = (float)ix/(float)(nx-1);
-         for (iy=0; iy<ny; iy++) {
+         for (int iy=0; iy<ny; iy++) {
             py = (float)iy/(float)(nx-1);
-            for (iz=0; iz<nz; iz++) {
+            for (int iz=0; iz<nz; iz++) {
                pz = (float)iz/(float)(nx-1);
                a[WY][ix][iy][iz] = 10.0 - 80.0*fabs((zf/2.0) - pz - 0.05*sin(px*2.0*M_PI));
                if (a[WY][ix][iy][iz] < 0.0) a[WY][ix][iy][iz] = 0.0;
@@ -243,11 +244,12 @@ int main(int argc,char **argv) {
       }
    } else if (false) {
       // create a wavy shear layer of positive vorticity (fully 3d)
-      for (ix=0; ix<nx; ix++) {
+      const float zf = (float)(nz-1)/(float)(nx-1);
+      for (int ix=0; ix<nx; ix++) {
          px = (float)ix/(float)(nx-1);
-         for (iy=0; iy<ny; iy++) {
+         for (int iy=0; iy<ny; iy++) {
             py = (float)iy/(float)(nx-1);
-            for (iz=0; iz<nz; iz++) {
+            for (int iz=0; iz<nz; iz++) {
                pz = (float)iz/(float)(nx-1);
                a[WY][ix][iy][iz] = 10.0 - 80.0*fabs((zf/2.0) - pz - 0.05*sin(px*2.0*M_PI) - 0.025*sin(py*2.0*M_PI));
                if (a[WY][ix][iy][iz] < 0.0) a[WY][ix][iy][iz] = 0.0;
@@ -256,9 +258,9 @@ int main(int argc,char **argv) {
       }
    } else if (false) {
       // create a random field of vorticity
-      for (ix=0; ix<nx; ix++) {
-         for (iy=0; iy<ny; iy++) {
-            for (iz=0; iz<nz; iz++) {
+      for (int ix=0; ix<nx; ix++) {
+         for (int iy=0; iy<ny; iy++) {
+            for (int iz=0; iz<nz; iz++) {
                a[WX][ix][iy][iz] = 2.0*rand()/RAND_MAX - 1.0;
                a[WY][ix][iy][iz] = 2.0*rand()/RAND_MAX - 1.0;
                a[WZ][ix][iy][iz] = 2.0*rand()/RAND_MAX - 1.0;
@@ -271,10 +273,10 @@ int main(int argc,char **argv) {
    }
    if (false) {
       // create a tube of vorticity along x
-      for (ix=0; ix<nx; ix++) {
-         for (iy=0; iy<ny; iy++) {
+      for (int ix=0; ix<nx; ix++) {
+         for (int iy=0; iy<ny; iy++) {
             py = (float)iy/(float)(nx-1);
-            for (iz=0; iz<nz; iz++) {
+            for (int iz=0; iz<nz; iz++) {
                pz = (float)iz/(float)(nx-1);
                px = sqrt(pow(py-0.5,2) + pow(pz-0.3,2));
                if (px < 0.1) {
@@ -288,10 +290,10 @@ int main(int argc,char **argv) {
    }
    if (false) {
       // create a tube of vorticity along x
-      for (ix=0; ix<nx; ix++) {
-         for (iy=0; iy<ny; iy++) {
+      for (int ix=0; ix<nx; ix++) {
+         for (int iy=0; iy<ny; iy++) {
             py = (float)iy/(float)(nx-1);
-            for (iz=0; iz<nz; iz++) {
+            for (int iz=0; iz<nz; iz++) {
                pz = (float)iz/(float)(nx-1);
                px = sqrt(pow(py-0.4,2) + pow(pz-0.7,2));
                if (px < 0.1) {
@@ -305,9 +307,9 @@ int main(int argc,char **argv) {
    }
    if (false) {
       // create the atmospheric boundary layer - NEEDS WORK
-      for (ix=0; ix<nx; ix++) {
-         for (iy=0; iy<ny; iy++) {
-            for (iz=0; iz<nz; iz++) {
+      for (int ix=0; ix<nx; ix++) {
+         for (int iy=0; iy<ny; iy++) {
+            for (int iz=0; iz<nz; iz++) {
                if ((float)(iz)/(0.2*(nz-1)) < 1.)
                   a[WY][ix][iy][iz] = 10.0*(0.5*cos((M_PI*iz)/(0.2*(nz-1)))+0.5);
             }
@@ -338,11 +340,11 @@ int main(int argc,char **argv) {
    }
 
    // set initial scalar
-   for (i=1; i<MAX_SCALARS; i++) if (sc_type[i] == SF) sfi = i;
+   for (int i=1; i<MAX_SCALARS; i++) if (sc_type[i] == SF) sfi = i;
    if (use_SF) {
-      for (ix=0; ix<nx; ix++) {
-         for (iy=0; iy<ny; iy++) {
-            for (iz=0; iz<nz; iz++) {
+      for (int ix=0; ix<nx; ix++) {
+         for (int iy=0; iy<ny; iy++) {
+            for (int iz=0; iz<nz; iz++) {
                a[sfi][ix][iy][iz] = 0.0;
             }
          }
@@ -362,9 +364,9 @@ int main(int argc,char **argv) {
       }
       if (true) {
          // columns of scalar
-         for (ix=0; ix<nx; ix++) {
-            for (iy=0; iy<ny; iy++) {
-               for (iz=0; iz<nz; iz++) {
+         for (int ix=0; ix<nx; ix++) {
+            for (int iy=0; iy<ny; iy++) {
+               for (int iz=0; iz<nz; iz++) {
                   pz = (float)iz/(float)(nz-1);
                   a[sfi][ix][iy][iz] = 4.8*MAX(0.,-cos(2.*M_PI*pz) + 0.01*rand()/RAND_MAX - 0.8);
                }
@@ -373,10 +375,10 @@ int main(int argc,char **argv) {
       }
       if (false) {
          // columns of scalar
-         //for (ix=nx/2; ix<nx; ix++) {
-         for (ix=0; ix<nx; ix++) {
-            for (iy=0; iy<ny; iy++) {
-               for (iz=0; iz<nz; iz++) {
+         //for (int ix=nx/2; ix<nx; ix++) {
+         for (int ix=0; ix<nx; ix++) {
+            for (int iy=0; iy<ny; iy++) {
+               for (int iz=0; iz<nz; iz++) {
                   //a[sfi][ix][iy][iz] = 1.0;
                   a[sfi][ix][iy][iz] = sin(ix*0.7)*sin(iy*0.5) +
                                        0.2*rand()/RAND_MAX - 0.1;
@@ -386,11 +388,11 @@ int main(int argc,char **argv) {
       }
       if (false) {
          // one column of scalar
-         for (ix=0; ix<nx; ix++) {
+         for (int ix=0; ix<nx; ix++) {
             px = (float)ix/(float)(nx-1);
-            for (iy=0; iy<ny; iy++) {
+            for (int iy=0; iy<ny; iy++) {
                py = (float)iy/(float)(ny-1);
-               for (iz=0; iz<nz; iz++) {
+               for (int iz=0; iz<nz; iz++) {
                   temp = sqrt(pow(0.5-px,2)+pow(0.5-py,2));
                   temp += 0.02*rand()/RAND_MAX - 0.01;
                   if (temp < 0.11) {
@@ -410,16 +412,16 @@ int main(int argc,char **argv) {
       pnum = 20*20*20;
       // allocate memory for particles
       ploc = (float **)malloc(sizeof(float *)*pnum);
-      for (i=0;i<pnum;i++) ploc[i]=(float *)malloc(sizeof(float)*3);
+      for (int i=0;i<pnum;i++) ploc[i]=(float *)malloc(sizeof(float)*3);
       pvel = (float **)malloc(sizeof(float *)*pnum);
-      for (i=0;i<pnum;i++) pvel[i]=(float *)malloc(sizeof(float)*3);
+      for (int i=0;i<pnum;i++) pvel[i]=(float *)malloc(sizeof(float)*3);
       prad = (float *)malloc(sizeof(float)*pnum);
 
       // place them
-      cnt = 0;
-      for (ix=0; ix<20; ix++) {
-         for (iy=0; iy<20; iy++) {
-            for (iz=0; iz<20; iz++) {
+      int cnt = 0;
+      for (int ix=0; ix<20; ix++) {
+         for (int iy=0; iy<20; iy++) {
+            for (int iz=0; iz<20; iz++) {
                ploc[cnt][0] = 0.45+0.1*((float)(ix)+0.5)/20.;
                ploc[cnt][1] = 0.45+0.1*((float)(iy)+0.5)/20.;
                ploc[cnt][2] = 0.0+0.1*((float)(iz)+0.5)/20.;
@@ -448,7 +450,7 @@ int main(int argc,char **argv) {
    //vmax = find_vmax(u[XV],u[YV],u[ZV],nx,ny,nz);
    // initialize the vmax averaging array
    vmax = courantconst / (dt*(nx+1));
-   for (i=0; i<VMAXAVG; i++) ccnvmax[i] = vmax;
+   for (int i=0; i<VMAXAVG; i++) ccnvmax[i] = vmax;
 
 
    // ----------------------------------
@@ -523,7 +525,6 @@ int main(int argc,char **argv) {
          // sprintf(outfileroot,"u_%04d",step);
          // write_output_3d(outfileroot,nx,ny,nz,u[XV],-0.5,1.0,outscale,false);
          // sprintf(outfileroot,"v_%04d",step);
-
          // write_output_3d(outfileroot,nx,ny,nz,u[YV],-0.5,1.0,outscale,false);
          // sprintf(outfileroot,"w_%04d",step);
          // write_output_3d(outfileroot,nx,ny,nz,u[ZV],-0.5,1.0,outscale,false);
@@ -544,10 +545,10 @@ int main(int argc,char **argv) {
          ccnidx = (ccnidx+1)%VMAXAVG;
          ccnvmax[ccnidx] = vmax;
          // if any were zero, reset them
-         for (i=0; i<VMAXAVG; i++) if (ccnvmax[i] < 1.e-6) ccnvmax[i] = vmax;
+         for (int i=0; i<VMAXAVG; i++) if (ccnvmax[i] < 1.e-6) ccnvmax[i] = vmax;
          // find new thing
          vmax = 0.0;
-         for (i=0; i<VMAXAVG; i++) vmax += ccnvmax[i];
+         for (int i=0; i<VMAXAVG; i++) vmax += ccnvmax[i];
          vmax /= (float)VMAXAVG;
          // recalculate dt
          if ((courantconst/vmax)/(nx+1) < 1.5*dt) {
@@ -562,7 +563,7 @@ int main(int argc,char **argv) {
       gettimeofday(&t_last, 0);
 
       // take one computational step forward in time
-      Re = step_forward_3d(step,isStam,
+      effRe = step_forward_3d(step,isStam,
               nx,ny,nz,xbdry,ybdry,zbdry,
               u,a,t,
               sc_cnt,sc_type,sc_diffus,
@@ -581,11 +582,11 @@ int main(int argc,char **argv) {
 
       // find effective diffusion
       nu_eff = (ke_last-ke)/(2.*enst*dt);
-      printf("  effective nu %g  explicit nu %g\n",nu_eff,1./reynolds);
+      printf("  effective nu %g  explicit nu %g\n",nu_eff,1./Re);
 
       // move the particles
       if (useParticles) {
-        explicit_particle_move_3d(nx,ny,nz,xbdry,ybdry,zbdry,u,dt,Re,pnum,ploc,pvel);
+        explicit_particle_move_3d(nx,ny,nz,xbdry,ybdry,zbdry,u,dt,effRe,pnum,ploc,pvel);
       }
 
       // calculate the total time elapsed, and the time for this last step
@@ -594,11 +595,11 @@ int main(int argc,char **argv) {
 
       if (writeOutput) {
          tottemp += walltime;
-         avgtemp = tottemp/(step+1);
+         //float avgtemp = tottemp/(step+1);
          // fprintf(stdout,"  took %g sec\n",temp);
          // fprintf(stdout,"  total %g and mean %g sec\n",tottemp,avgtemp);
-         //fprintf(stdout,"  mean step time: %.3g sec, effective Re %g\n",avgtemp,Re);
-         fprintf(stdout,"  step time: %.4g sec, effective Re %g\n", walltime, Re);
+         //fprintf(stdout,"  mean step time: %.3g sec, effective Re %g\n",avgtemp,effRe);
+         fprintf(stdout,"  step time: %.4g sec, effective Re %g\n", walltime, effRe);
       }
 
       // ----------------------------
